66a1d0281aee584fd183b535916f85026433d08f
afellow82 <76851912+afellow82@users.noreply.github.com>
2022-10-01 13:28:05 +0200

    Add files via upload

--- yourScript.js
+++ yourScript.js
@@ -2,7 +2,7 @@
 
 async function main() {
 	let widget = new ListWidget();
-  widget.addText('Widget Text');
+  widget.addText('Widget Test Jens');
  	let value = (config.runsInWidget) ? Script.setWidget(widget) : await widget.presentSmall();
  	Script.complete();
 }
