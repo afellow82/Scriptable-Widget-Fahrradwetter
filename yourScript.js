065d137c216775e9ff0606fbc0cc35cc3abc05fb
afellow82 <76851912+afellow82@users.noreply.github.com>
2022-10-01 14:26:09 +0200

    Update yourScript.js

--- yourScript.js
+++ yourScript.js
@@ -2,9 +2,14 @@
 
 async function main() {
 	let widget = new ListWidget();
-  widget.addText('Widget Test Jens');
+  widget.addText('Hauptfunktion');
+	aufruf();
  	let value = (config.runsInWidget) ? Script.setWidget(widget) : await widget.presentSmall();
  	Script.complete();
+}
+
+function aufruf () {
+ widget.addText('Aufruffunktion');	
 }
 
 module.exports = {
