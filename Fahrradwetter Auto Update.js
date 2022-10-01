63d82033f1b8548063982a25300ff50c5d7e7b59
afellow82 <76851912+afellow82@users.noreply.github.com>
2022-10-01 14:59:31 +0200

    Update Fahrradwetter Auto Update.js

--- Fahrradwetter Auto Update.js
+++ Fahrradwetter Auto Update.js
@@ -1,10 +1,9 @@
 // jshint -W119
 
 async function main() {
-	let widget = new ListWidget();
+  let widget = new ListWidget();
   widget.addText('Widget Test Jens');
- 	let value = (config.runsInWidget) ? Script.setWidget(widget) : await widget.presentSmall();
- 	Script.complete();
+  widget.presentSmall();
 }
 
 module.exports = {
